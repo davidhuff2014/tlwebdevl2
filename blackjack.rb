87774cb87145b766e092822fe50f00e874c22950
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-13 12:59:17 -0400

    lunchtime effort

--- blackjack.rb
+++ blackjack.rb
@@ -1,13 +1,28 @@
+# encoding: UTF-8
+module Hand
+  # who is holding what? 
+end
+
 class Blackjack
+  # game play?
 end
 
 class Cards
+  # inherits from hand?
 end
 
 class Deck
+  attr_accessor :deck
+suits = ['C', 'S', 'D', 'H']
+
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+$deck = cards.product(suits) # prefer my cards called by number then suit
+$deck.shuffle!
 end
 
 class Dealer
+  # rules and gate keeper?
 end
 
 class Player
@@ -16,3 +31,11 @@ class Player
     @name = n
   end
 end
+
+class Scores
+end
+
+Deck.new
+p $deck
+p1 = Player.new('Dave')
+p p1.name
