a38e87a7cebd2aa7d3ce081f1c06604dffb17a2c
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-12 21:43:45 -0400

    first push of pseudo code

---
+++ game_of_21_pseudo_code.rb
@@ -0,0 +1,19 @@
+# blackjack the game itself
+# cards
+# dealer
+# player
+# dealer stays >= 17
+# 21 is blackjack, dealer wins if tied if both have blackjack
+# create the cards
+# merge the cards
+# shuffle the cards
+# take player name
+# take player wager
+# scoff at player
+# deal cards
+# request hit or stay from player
+# accumulate scores
+# deal to dealer if dealer score < 17
+# compare scores under 21
+# declare winner
+# take money from player
