bfcd3db796817d1f3f2293b6c162ff4c3d8072d1
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-16 20:23:39 -0400

    Lesson2 Quiz responses

---
+++ Lesson2_Quiz.rb
@@ -0,0 +1,27 @@
+# value is a variable , 1 is an integer
+# @2a is an instance variable being set to a 2 (fixnum)
+# user = User.new is creating a note instance of class User
+# user.name is the name instance (object) of class user
+# user.name = 'Joe' setting the instance variable name to Joe
+# by calling the instance method name usint the setter method
+
+# use include to mixin a module
+
+# class variables are only used inside the class
+# instance variables can be called outside the class but not inherited
+
+# attr_accessor is a getter and setter method
+
+# Dog.some_method is calling a class method
+
+#  def initialize(n)
+#    @name = n
+#  end
+
+# use subclass to inherit variables and methods
+# use mixin module where multiple classes can calling
+
+# yes you can call instance methods within a class
+
+# require 'pry'
+# binding.pry
